7bcc495a4fb32b3b63f112c52a9c0751b95143c0
GauthierWebDev <gauthier@gauthierdaniels.fr>
2024-07-09 20:56:24 +0200

    docs: Add link to generated index in database category

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,15 +9,15 @@ const config: Config = {
   favicon: "img/favicon.webp",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://memento-dev.fr",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "preparation-tp", // Usually your GitHub org/user name.
+  projectName: "memento-dev", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -114,6 +114,10 @@ const config: Config = {
             {
               label: "Préambule",
               to: "/docs/intro",
+            },
+            {
+              label: "Bases de données",
+              to: "/docs/category/-bases-de-données",
             },
           ],
         },
