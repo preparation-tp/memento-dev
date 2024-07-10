9f24fe1bb4764f1aafc390228b83555237e90600
Gauthier Daniels <45950830+GauthierWebDev@users.noreply.github.com>
2024-07-10 12:35:05 +0200

    Feat/db modelisation guide (#5)
    * docs: Add documentation for different data documents
    
    * feat: Add ICalLink component for creating Google Calendar event links
    
    * style: Fix button styling and event type in ICalLink
    
    * style: Update custom.css variables for quote admonition

---
+++ src/components/ICalLink/index.tsx
@@ -0,0 +1,54 @@
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+
+type ICalLinkProps = {
+  className?: string;
+  date: string;
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+const ICalLink = (props: ICalLinkProps) => {
+  const createGoogleCalendarLink = () => {
+    const startDate = new Date(props.date)
+      .toISOString()
+      .replace(/-|:|\.\d+/g, "");
+
+    const endDate = new Date(props.date)
+      .toISOString()
+      .replace(/-|:|\.\d+/g, "");
+
+    const descriptionEncoded = encodeURIComponent(props.description || "");
+    const titleEncoded = encodeURIComponent(props.title);
+
+    return `https://www.google.com/calendar/render?action=TEMPLATE&text=${titleEncoded}&dates=${startDate}/${endDate}&details=${descriptionEncoded}`;
+  };
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    const link = createGoogleCalendarLink();
+    window.open(link, "_blank");
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={clsx(
+        "font-bold",
+        "inline-flex",
+        "gap-2",
+        "items-center",
+        "transition-colors",
+        "hover:text-violet-300",
+        props.className
+      )}
+    >
+      📅 {props.children} 📅
+    </button>
+  );
+};
+
+export default ICalLink;
