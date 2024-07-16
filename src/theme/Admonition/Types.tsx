42ce5ecd918f1ce81b45a8ad85ddea611b8ea5f5
GauthierWebDev <gauthier@gauthierdaniels.fr>
2024-07-16 13:04:47 +0200

    style: Improve class names for Admonition icons

--- src/theme/Admonition/Types.tsx
+++ src/theme/Admonition/Types.tsx
@@ -17,7 +17,7 @@ const QuoteAdmonition = (props: QuoteAdmonitionProps) => {
           fill="currentColor"
           strokeWidth="0"
           viewBox="0 0 24 24"
-          className="inline-block w-4 mr-2"
+          className="inline-block w-4 mr-2 shrink-0"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"></path>
@@ -40,7 +40,7 @@ const ExampleAdmonition = (props: QuoteAdmonitionProps) => {
           fill="currentColor"
           strokeWidth="0"
           viewBox="0 0 256 256"
-          className="inline-block w-4 mr-2"
+          className="inline-block w-4 mr-2 shrink-0"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M237.66,86.34l-60-60a8,8,0,0,0-11.32,0L37.11,155.57a44.77,44.77,0,0,0,63.32,63.32L212.32,107l22.21-7.4a8,8,0,0,0,3.13-13.25Zm-32.19,6.07a8,8,0,0,0-3.13,1.93l-39.57,39.57c-8.47,2.9-21.75,4-39.07-5-10.6-5.54-20.18-8-28.56-8.73L172,43.31,217.19,88.5Z"></path>
