f04337fba2cebc831ec0cda4578f65eee73e1e17
GauthierWebDev <gauthier@gauthierdaniels.fr>
2024-11-18 14:09:03 +0100

    feat: Introduce testimonial carousel with navigation buttons

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,21 +1,61 @@
 import type { Testimonial } from "../../data/testimonials";
+import type { Options } from "react-markdown";
 
 import TestimonialCardButton from "../TestimonialCardButton";
 import testimonials from "../../data/testimonials";
-import { useState, useEffect } from "react";
+import { useState, useMemo } from "react";
+import Markdown from "react-markdown";
 import clsx from "clsx";
+
+const markdownOptions: Options = {
+  allowedElements: ["strong", "em", "p"],
+};
 
 const Testimonials = () => {
   const [selectedTestimonial, setSelectedTestimonial] =
     useState<Testimonial | null>(null);
+  const [isOpened, setIsOpened] = useState(false);
 
   const handleCloseFromContainer = (
     event: React.MouseEvent<HTMLDivElement>
   ) => {
     if (event.target === event.currentTarget) {
-      setSelectedTestimonial(null);
+      setIsOpened(null);
     }
   };
+
+  const changeSelectedTestimonial = (testimonial: Testimonial) => {
+    setSelectedTestimonial(testimonial);
+    setIsOpened(true);
+  };
+
+  const previousSelectedTestimonial = useMemo(() => {
+    if (!selectedTestimonial) return null;
+
+    const currentTestimonialIndex = testimonials.findIndex(
+      (testimonial) => testimonial === selectedTestimonial
+    );
+
+    const previousTestimonialIndex =
+      (currentTestimonialIndex - 1 + testimonials.length) % testimonials.length;
+
+    return testimonials[previousTestimonialIndex] || null;
+  }, [selectedTestimonial]);
+
+  const nextSelectedTestimonial = useMemo(() => {
+    if (!selectedTestimonial) return null;
+
+    const currentTestimonialIndex = testimonials.findIndex(
+      (testimonial) => testimonial === selectedTestimonial
+    );
+
+    const nextTestimonialIndex =
+      (currentTestimonialIndex + 1) % testimonials.length;
+
+    return testimonials[nextTestimonialIndex] || null;
+  }, [selectedTestimonial]);
+
+  const shouldShowPreviousAndNextButtons = testimonials.length > 1;
 
   return (
     <>
@@ -23,7 +63,7 @@ const Testimonials = () => {
         {testimonials.map((testimonial, index) => (
           <li className="!m-0" key={index}>
             <TestimonialCardButton
-              setSelectedTestimonial={setSelectedTestimonial}
+              setSelectedTestimonial={changeSelectedTestimonial}
               testimonial={testimonial}
             />
           </li>
@@ -33,6 +73,9 @@ const Testimonials = () => {
       <div
         className={clsx(
           "fixed",
+          "flex",
+          "items-center",
+          "justify-center",
           "w-screen",
           "h-screen",
           "bg-black/50",
@@ -44,14 +87,118 @@ const Testimonials = () => {
           "no-mt",
           "transition-opacity",
           "duration-300",
-          { "opacity-0": !selectedTestimonial },
-          { "opacity-100": selectedTestimonial },
-          { "pointer-events-none": !selectedTestimonial },
-          { "pointer-events-auto": selectedTestimonial }
+          { "opacity-0": !isOpened },
+          { "opacity-100": isOpened },
+          { "pointer-events-none": !isOpened },
+          { "pointer-events-auto": isOpened }
         )}
         onClick={handleCloseFromContainer}
       >
-        <div></div>
+        {isOpened && selectedTestimonial && (
+          <section className="relative max-w-full max-h-[calc(100vh-10rem)] w-max p-4 rounded dark:bg-gray-800 bg-gray-200 overflow-auto">
+            <button
+              title="Fermer la fenêtre"
+              onClick={() => setIsOpened(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={() => setIsOpened(false)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+
+            <header className="flex items-center gap-8 justify-between mb-6">
+              {previousSelectedTestimonial &&
+                shouldShowPreviousAndNextButtons && (
+                  <button
+                    className="button"
+                    onClick={() =>
+                      setSelectedTestimonial(previousSelectedTestimonial)
+                    }
+                    title="Témoignage précédent"
+                  >
+                    {"<"}
+                  </button>
+                )}
+
+              <div className="grow w-full text-center">
+                <h2>
+                  {selectedTestimonial.name &&
+                    `Témoignage de ${selectedTestimonial.name}`}
+                  {!selectedTestimonial.name && "Témoignage anonyme"}
+                </h2>
+                <time
+                  className="italic text-sm"
+                  dateTime={selectedTestimonial?.date.toISOString()}
+                >
+                  Rédigé le{" "}
+                  {selectedTestimonial.date.toLocaleDateString("fr-FR", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </time>
+              </div>
+
+              {nextSelectedTestimonial && shouldShowPreviousAndNextButtons && (
+                <button
+                  className="button"
+                  onClick={() =>
+                    setSelectedTestimonial(nextSelectedTestimonial)
+                  }
+                  title="Témoignage suivant"
+                >
+                  {">"}
+                </button>
+              )}
+            </header>
+
+            <div className="grid md:grid-cols-3 gap-8">
+              <section>
+                <h3>Avant la soutenance</h3>
+                <div className="p-2 rounded dark:bg-gray-700 bg-gray-300">
+                  {selectedTestimonial.before.map((paragraph, index) => (
+                    <Markdown key={index} {...markdownOptions}>
+                      {paragraph}
+                    </Markdown>
+                  ))}
+                </div>
+              </section>
+
+              <section>
+                <h3>Pendant la soutenance</h3>
+                <div className="p-2 rounded dark:bg-gray-700 bg-gray-300">
+                  {selectedTestimonial.during.map((paragraph, index) => (
+                    <Markdown key={index} {...markdownOptions}>
+                      {paragraph}
+                    </Markdown>
+                  ))}
+                </div>
+              </section>
+
+              <section>
+                <h3>Après la soutenance</h3>
+                <div className="p-2 rounded dark:bg-gray-700 bg-gray-300">
+                  {selectedTestimonial.after.map((paragraph, index) => (
+                    <Markdown key={index} {...markdownOptions}>
+                      {paragraph}
+                    </Markdown>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </section>
+        )}
       </div>
     </>
   );
--- src/data/testimonials.tsx
+++ src/data/testimonials.tsx
@@ -8,7 +8,7 @@ export type Testimonial = {
 
 const testimonials: Testimonial[] = [
   {
-    name: "Christian H.",
+    name: "Christian HUMBERT",
     date: new Date("2024-11-18"),
     before: [
       "Comme j'ai l'habitude de donner des conférences, me retrouver devant un jury ne me faisait pas très peur.",
@@ -23,6 +23,23 @@ const testimonials: Testimonial[] = [
       'Tu te dis "J\'ai gagné la finale !!".',
       "Tant de temps de formation, des heures de travail pour écrire les dossiers...",
       "Le tout, c'est d'être fier de ton taf, quelque soit le résultat. Et même si tu ne l'as pas du premier coup, les échecs forments à la réussite 😉",
+    ],
+  },
+  {
+    date: new Date("2024-11-18"),
+    before: [
+      "Stress !!!",
+      "C'était vraiment l'inconnu pour moi et je me rappelle m'être dit qu'en plus de six mois, je ne suis plus derrière un écran mais je vais bien être évalué par des professionnels, physiquement, sur un lieu de rendez-vous bien réel !",
+      "Je peux ajouter que j'avais quand même un sentiment d'être préparé et d'avoir vraiment travaillé dur entre la fin de ma formation et le TP.",
+    ],
+    during: [
+      "C'est allé assez vite une fois qu'on y est. Tout était programmé dans ma tête, je savais que j'allais :",
+      "Me présenter, présenter mon projet puis répondre aux questions.",
+      "Lors des questions, j'ai été assez frustré par moment car je connaissais la réponse, mais je ne savais pas comment l'expliquer.",
+      "Le jury a été bienveillant et a essayé de me guider sans me donner la réponse pour autant.",
+    ],
+    after: [
+      "Sentiment de soulagement sur 2-3 jours, puis montage russe d'émotions car le processus de reconversion n'est pas fini.",
     ],
   },
 ];
